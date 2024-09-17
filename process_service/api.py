660c69410615881ae447629ab3e87972877ea7e4
trongthanht3 <trongthanht3@gmail.com>
2024-09-17 18:53:24 +0700

    update service worker

--- process_service/api.py
+++ process_service/api.py
@@ -1,13 +1,13 @@
 import re
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 import fitz
 import pandas as pd
 import json
 
 app = FastAPI()
 
-def process_data(data):
+async def process_data(data):
     # Regex pattern to identify valid date (dd/mm/yyyy)
     date_pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
     
@@ -41,20 +41,30 @@ def process_data(data):
     # Display the reformatted DataFrame
     return new_df
 
-def process_balance(input_text):
+async def process_balance(input_text):
     input_text = input_text.strip()
     input_text = input_text.split(' ')[0]
     input_text = input_text.replace('.', '')
     input_text = input_text.strip()
     return int(input_text)
 
+async def print_request(request):
+    print(f'request header       : {dict(request.headers.items())}' )
+    print(f'request query params : {dict(request.query_params.items())}')  
+    try : 
+        print(f'request json         : {await request.json()}')
+    except Exception as err:
+        # could not parse json
+        print(f'request body         : {await request.body()}')
+
+
 # httpoption check for haproxy
 @app.options("/")
 async def options():
     return {"status": "ok"}
 
-@app.post("/uploadfile/")
-async def upload_file(file_input: UploadFile = File(...)):
+@app.post("/uploadfile")
+async def upload_file(request: Request, file_input: UploadFile = File(...)):
     # f = open(file_input.file)
     doc = fitz.open(stream=file_input.file.read(), filetype='pdf') # open a document
     page = doc[0]
@@ -73,15 +83,15 @@ async def upload_file(file_input: UploadFile = File(...)):
         df.loc[-1] = old_column_names  # Add new row at index -1
         df.index = df.index + 1        # Shift index
         df = df.sort_index()           # Sort index to make the new row the first one
-        new_df = process_data(df.values)
+        new_df = await process_data(df.values)
         df_raw = pd.concat([df_raw, new_df], ignore_index=True)
     except Exception as e:
         print(df)
-        for i,tab in enumerate(tabs):  # iterate over all tables
-            for cell in tab.header.cells:
-                page.draw_rect(cell,color=fitz.pdfcolor["red"],width=0.3)
-            page.draw_rect(tab.bbox,color=fitz.pdfcolor["green"])
-            print(f"Table {i} column names: {tab.header.names}, external: {tab.header.external}")
-            old_column_names = tab.header.names
-            print(old_column_names)
+        # for i,tab in enumerate(tabs):  # iterate over all tables
+            # for cell in tab.header.cells:
+            #     page.draw_rect(cell,color=fitz.pdfcolor["red"],width=0.3)
+            # page.draw_rect(tab.bbox,color=fitz.pdfcolor["green"])
+            # print(f"Table {i} column names: {tab.header.names}, external: {tab.header.external}")
+            # old_column_names = tab.header.names
+            # print(old_column_names)
     return json.loads(df_raw.to_json(orient='records'))
